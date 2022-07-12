df48112bfeb75ffb945d5cc29f1a1e9aba0439dc
Mechi van Keulen <mercedesvankeulen@gmail.com>
2022-07-11 21:54:12 -0300

    PequeÃños arreglos en test

--- src/features/quote/Cita.test.tsx
+++ src/features/quote/Cita.test.tsx
@@ -6,29 +6,15 @@ import { NO_ENCONTRADO, NOMBRE_INVALIDO, MENSAJE_CARGANDO } from "./constants";
 
 describe("Cita", () => {
   describe("Cuando renderizamos el componente", () => {
-    test("No deberia mostrar ninguna cita", async () => {
+    test("No debe mostrar ninguna cita", async () => {
       render(<Cita />);
       const cita = screen.queryByText(NO_ENCONTRADO);
       expect(cita).toBeInTheDocument();
     });
   });
 
-  describe("Cuando la query se esta ejecutando", () => {
-    test("Deberia mostrar el mensaje de cargando", async () => {
-      render(<Cita />);
-
-      const button = screen.queryByText("Obtener cita aleatoria");
-      if (button) fireEvent.click(button);
-
-      await waitFor(() => {
-        const mensajeCargando = screen.queryByText(MENSAJE_CARGANDO);
-        expect(mensajeCargando).toBeInTheDocument();
-      });
-    });
-  });
-
   describe("Cuando se ingresa un valor en el input", () => {
-    test('Deberia cambiar la label del boton a "Obtener Cita"', async () => {
+    test('Debe cambiar el texto del boton a "Obtener Cita"', async () => {
       render(<Cita />);
 
       const input = screen.queryByPlaceholderText(
@@ -40,7 +26,6 @@ describe("Cita", () => {
         const buttonCita = screen.queryByText("Obtener Cita");
         expect(buttonCita).toBeInTheDocument();
       });
-
       await waitFor(() => {
         const buttonCitaAleatoria = screen.queryByText(
           "Obtener cita aleatoria"
@@ -49,7 +34,7 @@ describe("Cita", () => {
       });
     });
 
-    test("Deberia cambiar el valor del input al valor ingresado", async () => {
+    test("Debe cambiar el valor del input al valor ingresado", async () => {
       render(<Cita />);
 
       const input = screen.queryByPlaceholderText(
@@ -63,8 +48,22 @@ describe("Cita", () => {
     });
   });
 
+  describe("Cuando la query se esta ejecutando", () => {
+    test("Debe mostrar el mensaje de cargando", async () => {
+      render(<Cita />);
+
+      const button = screen.queryByText("Obtener cita aleatoria");
+      if (button) fireEvent.click(button);
+
+      await waitFor(() => {
+        const mensajeCargando = screen.queryByText(MENSAJE_CARGANDO);
+        expect(mensajeCargando).toBeInTheDocument();
+      });
+    });
+  });
+
   describe("Cuando se borra el contenido del input", () => {
-    it("No deberia mostrarse ninguna cita", async () => {
+    it("No debe mostrarse ninguna cita", async () => {
       render(<Cita />);
 
       const inputChar = screen.getByPlaceholderText(
@@ -79,7 +78,7 @@ describe("Cita", () => {
       expect(await screen.findByText(NO_ENCONTRADO)).toBeInTheDocument();
     });
 
-    test("Deberia cambiar el valor del input a vacio", async () => {
+    test("Debe cambiar el valor del input a vacio", async () => {
       render(<Cita />);
       const input = screen.queryByPlaceholderText(
         "Ingresa el nombre del autor"
@@ -96,8 +95,8 @@ describe("Cita", () => {
     });
   });
 
-  describe("Cuando la query se ejecuta correctamente", () => {
-    it("Deberia mostrar una cita del personaje ingresado", async () => {
+  describe("Cuando se ingresa un input valido", () => {
+    it("Debe mostrar una cita del personaje ingresado", async () => {
       render(<Cita />);
       const inputChar = screen.getByPlaceholderText(
         "Ingresa el nombre del autor"
@@ -109,8 +108,8 @@ describe("Cita", () => {
     });
   });
 
-  describe("Cuando la query se ejecuta erroneamente", () => {
-    it("Deberia mostrar un mensaje de error", async () => {
+  describe("Cuando se ingresa un input invalido", () => {
+    it("Debe mostrar un mensaje de error", async () => {
       render(<Cita />);
       const inputChar = screen.getByPlaceholderText(
         "Ingresa el nombre del autor"
